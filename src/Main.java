ae70d0ec1f113a0ec00b134c86d4d153c27b026f
Abraham <xxu@wccnet.edu>
2018-02-11 15:36:51 -0500

    No.2 Version
    This is the second version

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("This is the No.1 version! Wrote at 3:08");
+        System.out.println("This is the No.2 version! Wrote at 3:36");
     }
 }
