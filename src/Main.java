c65fe919b73c11d3a57c77f224c449dab83575db
Abraham <xxu@wccnet.edu>
2018-02-11 18:14:27 -0500

    No we revert to the sixth version AGAIN!
    Now the circle is done!

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         System.out.println("This is the No.1 version! Wrote at 3:08");
         System.out.println("This is the No.2 version! Wrote at 3:36");
         System.out.println("This is the No.3 version! Wrote at 3:41");
-
+        System.out.println("This is the No.4 version! Wrote at 4:55");
+        System.out.println("This is the No.5 version! Wrote at 5:00 and it has some problem! We need to retreat to the third version");
+        System.out.println("This is the No.6 version! Just for version control");
     }
 }
