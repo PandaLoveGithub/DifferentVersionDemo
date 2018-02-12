a27ae8aebeeb0c87335f5a9afdcff2da81ffb376
Abraham <xxu@wccnet.edu>
2018-02-11 19:22:56 -0500

    This is the final correct version. I still do not know how to revert directly on Github, sigh~

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println("This is the No.1 version! Wrote at 3:08");
         System.out.println("This is the No.2 version! Wrote at 3:36");
         System.out.println("This is the No.3 version! Wrote at 3:41");
