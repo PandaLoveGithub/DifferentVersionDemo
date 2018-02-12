bacd7c8887c855245759e850391661baa3405fcb
Abraham <xxu@wccnet.edu>
2018-02-12 12:23:20 -0500

    Now we go back to the third version at Feb 12

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is the No.1 version! Wrote at 3:08");
-        System.out.println("This is the No.2 version! Wrote at 3:36");
-        System.out.println("This is the No.3 version! Wrote at 3:41");
-        System.out.println("This is the No.4 version! Wrote at 4:55");
-        System.out.println("This is the No.5 version! Wrote at 5:00 and it has some problem! We need to retreat to the third version");
-        System.out.println("This is the No.6 version! Just for version control");
+
+        System.out.println("This is the No.1 version!");
+        System.out.println("This is the No.2 version! Updated at 2:51 pm");
+        System.out.println("This is the No.3 version! Updated at 2:55 pm");
     }
 }
